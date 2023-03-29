38986c847ba0f50b57cfe4ef643f7c739a5d752e
lucasDBH1 <129281734+lucasDBH1@users.noreply.github.com>
2023-03-29 12:53:31 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
-    led.plot(1, 1)
-    led.plot(2, 2)
-    led.plot(3, 3)
-    led.plot(4, 4)
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
 })
